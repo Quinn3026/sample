7f61f29c78a9320931ef761cbd68cdf196e38e20
Quinn3026 <gacayanquinn22222@gmail.com>
2022-07-10 00:53:25 +0800

    1st Commit

---
+++ flowc_app/android/app/src/main/kotlin/com/example/flowc_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flowc_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flowc_app/lib/main.dart
@@ -0,0 +1,17 @@
+//import 'dart:io';
+
+
+
+void main() {
+
+  int r = 6;
+
+
+  for (int i=0; i<r; i++){
+    for(int j=0; j<=i; j++){
+      print('* ');
+    }
+    //print('\n');
+  }
+}
+
